dd091bbf655ae34de81996286fd612031e41ae79
Taylor Balding <taylorbalding@me.com>
2025-01-21 15:32:25 -0700

    update

--- Taylor.py
+++ Taylor.py
@@ -5,3 +5,14 @@ print ("Hello")
 print (2+2)
 print(type("Hello, World!"))
 print(len ("Hello world"))
+print(2*6)
+print(type("Hello world"))
+pi=3.1459
+print(pi*6)
+minutes=645
+hours=minutes//60
+print(hours)
+print (int(3.56))
+print (float(173))
+print (str(196))
+n=input("Please enter your name")
