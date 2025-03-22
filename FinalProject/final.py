02d993b0ea6961d3fb36ead2ad7e2071db783032
Taylor Balding <taylorbalding@me.com>
2025-03-22 15:33:08 -0700

    FinalWork

--- FinalProject/final.py
+++ FinalProject/final.py
@@ -9,6 +9,8 @@
 import random
 import turtle
 
+keep_playing = True
+
 #Setting up the turtle screen for hangman
 wn=turtle.Screen()
 wn.bgcolor("Black")
@@ -63,8 +65,6 @@ def create_hangman(mistakes):
         pen.goto(50, 20)
         pen.pendown()
         pen.goto(70, 0)
-        turtle.done()
-        print("Gameover. The correct word was {}".format(chosen_word))
 
 def choose_word():
     with open("FinalProject/wordlist.txt", "r" ) as wordfile:
@@ -72,43 +72,59 @@ def choose_word():
     wordfile.close()
     return random.choice(words).strip()
 
-chosen_word=choose_word()
-guessed_letters=[]
 
-correctcount = 0
-incorrectcount = 0
-mistakes = 0
+def playgame():
+    chosen_word=choose_word()
+    guessed_letters=[]
 
-while mistakes<=10:
-    guess=input("Guess a letter.").lower()
+    correctcount = 0
+    incorrectcount = 0
+    mistakes = 0
+
+    while mistakes<10:
+        guess=input("Guess a letter: ").lower()
+        
+        if not guess.isalpha():
+            print("Invalid character, please guess again!")
+            continue
     
-    if guess in guessed_letters:
-        print("You guessed that letter already. Guess again!")
-        continue
-    guessed_letters.append(guess)
+        if guess in guessed_letters:
+            print("You guessed that letter already. Guess again!")
+            continue
+        guessed_letters.append(guess)
 
-    if guess in chosen_word:
-        correctcount += 1
-        print("Correct!")
+        if guess in chosen_word:
+            correctcount += 1
+            print("Correct!")
+        
+        else:
+            incorrectcount += 1
+            mistakes += 1
+            print("Wrong guess.")
+            create_hangman(mistakes)
+        
+        word_guessed = all(letter in guessed_letters for letter in chosen_word)
+        if word_guessed:
+            print("Congratulations! You guessed your word: {}.".format(chosen_word))
+            break
+
+    if mistakes == 10: #Result when the player loses the game 
+        print ("The game is over. The correct word was {}".format(chosen_word))
+
+
+    with open("FinalProject/results.txt", "w") as resultfile:
+        resultfile.write("{}          {}\n".format("Correct","Incorrect"))
+        resultfile.write("==========================\n")
+        resultfile.write("{}...................{}".format(correctcount,incorrectcount))
     
-    else:
-        incorrectcount += 1
-        mistakes += 1
-        print("Wrong guess.")
-        create_hangman(mistakes)
-    
-    word_guessed = all(letter in guessed_letters for letter in chosen_word)
-    if word_guessed:
-        print("Congratulations! You guessed your word: {}.".format(chosen_word))
+    pen.clear()
+        
 
-if mistakes==10: #Result when the player loses the game 
-    print ("The game is over. The correct word was {}".format(chosen_word))
-
-
-
-with open("FinalProject/results.txt", "w") as resultfile:
-    resultfile.write("{}          {}\n".format("Correct","Incorrect"))
-    resultfile.write("==========================\n")
-    resultfile.write("{}.................{}".format(correctcount,incorrectcount))
+#Running code, everything above this is functions
+while keep_playing == True:
+    playgame() 
+    gameplay = str(input("Would you like to keep playing?(Y/N)"))
+    if gameplay == "N":
+        keep_playing = False   
 
 turtle.done
