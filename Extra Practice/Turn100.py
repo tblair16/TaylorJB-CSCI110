a713186aa2921d566ec4ff4574f103e176e34ca1
Taylor Balding <taylorbalding@me.com>
2025-04-07 09:11:54 -0600

    Practice

---
+++ Extra Practice/Turn100.py
@@ -0,0 +1,7 @@
+def year100():
+    age = int(input("How old are you?"))
+    name = input("What is your name?")
+    year = 2025 - age + 100
+    print(f"{name} You will turn 100 in {year}.")
+
+year100()
